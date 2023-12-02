51195c5176a76c2ce96280d6a3bf67fce26be63d
Luke <ldv432@gmail.com>
2023-12-01 21:43:10 -0700

    fetched

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Add your code here
+const data = {
+    name: "Steve",
+    email: "steve@steve.com"
+}
+
+const config = {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+    },
+    body: JSON.stringify(data)
+}
+
+function submitData(){
+return fetch('http://localhost:3000/users', config)
+    .then(res => res.json())
+    .then(obj => document.body.textContent = obj.id)
+    .catch(error => document.body.textContent = error.message)
+}
